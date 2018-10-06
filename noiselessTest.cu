1726c8a488453c34c7bd9861e5f797a93269a88b
Luca Tansini <luca.tansini@studenti.unimi.it>
2018-10-06 17:03:23 +0200

    sistemate free

--- noiselessTest.cu
+++ noiselessTest.cu
@@ -13,84 +13,77 @@ int main(int argc, char **argv){
     setbuf(stdout, NULL);
 
     int n = atoi(argv[1]);
-    int m = 2*n;
-    int k = atoi(argv[2]);
 
-    //DEBUG
-    printf("n:%d\tm:%d\tk:%d\n",n,m,k);
-    //END DEBUG
-
-    //CREA DIZIONARIO D
-    printf("creating dictionary D...");
-    double *D;
-    CHECK(cudaMalloc(&D, n*m*sizeof(double)));
-
-    createDict(D, n, m);
-    printf("done\n");
-
-    //CALCOLA PSEUDOINVERSA DINV
-    printf("computing pseudoinverse DINV...");
-    double *DINV;
-    CHECK(cudaMalloc(&DINV, m*n*sizeof(double)));
-
-    JacobiMoorePenroseInverse(D, n, m, DINV);
-    printf("done\n");
-
-    if(!CheckPseudoinverse(D, n, m, DINV))
-        printf("Something went wrong with the Moore-Penrose pseudoinverse!\n");
-
-    //GENERA ALPHAOPT
-    printf("generating alphaopt...");
-    double *alphaopt;
-    CHECK(cudaMalloc(&alphaopt, m*sizeof(double)));
-
-    generateAlpha(alphaopt, m, k);
-    printf("done\n");
-
-    //CALCOLA s = D * alphaopt
-    printf("computing s = D * alphaopt...");
-    double *s;
+    //ALLOCATE MEMORY POINTERS
+    double *D,*DINV,*alphaopt,*s,*alphalimaps,*h_alphalimaps,*h_alphaopt;
     CHECK(cudaMalloc(&s, n*sizeof(double)));
 
+    //CREATE CUBLAS HANDLE
     cublasHandle_t cublasHandle;
     CHECK_CUBLAS(cublasCreate(&cublasHandle));
     double cualpha=1,cubeta=0;
 
-    CHECK_CUBLAS(cublasDgemv(cublasHandle, CUBLAS_OP_N, n, m, &cualpha, D, n, alphaopt, 1, &cubeta, s, 1));
-    printf("done\n");
+    //CICLO SU M
+    for(int m = n; m <= 2*n; m += n/10){
 
-    //CHIAMA K_LiMapS
-    printf("calling K_LiMapS...");
-    double *alphalimaps;
-    CHECK(cudaMalloc(&alphalimaps, m*sizeof(double)));
+        //CREA DIZIONARIO
+        CHECK(cudaMalloc(&D, n*m*sizeof(double)));
+        createDict(D, n, m);
 
-    devMemK_LiMapS(k, D, n, m, DINV, s, alphalimaps, 1000);
-    printf("done\n");
+        //CALCOLA PSEUDOINVERSA
+        CHECK(cudaMalloc(&DINV, m*n*sizeof(double)));
+        JacobiMoorePenroseInverse(D, n, m, DINV);
+        if(!CheckPseudoinverse(D, n, m, DINV))
+            printf("Something went wrong with the Moore-Penrose pseudoinverse!\n");
 
-    //CHECK DEL RISULTATO
-    double *h_alphalimaps,*h_alphaopt;
-    CHECK(cudaMallocHost(&h_alphaopt, m*sizeof(double)));
-    CHECK(cudaMallocHost(&h_alphalimaps, m*sizeof(double)));
-    CHECK(cudaMemcpy(h_alphaopt, alphaopt, m*sizeof(double), cudaMemcpyDeviceToHost));
-    CHECK(cudaMemcpy(h_alphalimaps, alphalimaps, m*sizeof(double), cudaMemcpyDeviceToHost));
+        //ALLOCA alphaopt e alphalimaps
+        CHECK(cudaMalloc(&alphaopt, m*sizeof(double)));
+        CHECK(cudaMalloc(&alphalimaps, m*sizeof(double)));
+        CHECK(cudaMallocHost(&h_alphaopt, m*sizeof(double)));
+        CHECK(cudaMallocHost(&h_alphalimaps, m*sizeof(double)));
 
-    int i;
-    for(i=0; i<m; i++)
-        if(fabs(h_alphaopt[i] - h_alphalimaps[i]) > 1e-3)
-            break;
-    if(i == m)
-        printf("ALL GOOD\n");
-    else
-        printf("SOMETHING WENT WRONG!\n");
+        //CICLO SU K e iters
+        for(int k=n/10; k<=n/2; k+=n/20){
+            for(int iters=0; iters<50; iters++){
 
-    printf("MSE: %f\n", MSE(s,D,alphalimaps,n,m));
+                //GENERA alphaopt
+                generateAlpha(alphaopt, m, k);
+
+                //CALCOLA s = D * alphaopt
+                CHECK_CUBLAS(cublasDgemv(cublasHandle, CUBLAS_OP_N, n, m, &cualpha, D, n, alphaopt, 1, &cubeta, s, 1));
+
+                //CHIAMA K_LiMapS
+                devMemK_LiMapS(k, D, n, m, DINV, s, alphalimaps, 1000);
+
+                //CHECK DEL RISULTATO
+                CHECK(cudaMemcpy(h_alphaopt, alphaopt, m*sizeof(double), cudaMemcpyDeviceToHost));
+                CHECK(cudaMemcpy(h_alphalimaps, alphalimaps, m*sizeof(double), cudaMemcpyDeviceToHost));
+
+                int i;
+                for(i=0; i<m; i++)
+                    if(fabs(h_alphaopt[i] - h_alphalimaps[i]) > 1e-3)
+                        break;
+                if(i == m)
+                    printf("ALL GOOD\n");
+                else
+                    printf("SOMETHING WENT WRONG!\n");
+
+                printf("MSE: %f\n", MSE(s,D,alphalimaps,n,m));
+
+            }
+        }
+
+        CHECK(cudaFree(D));
+        CHECK(cudaFree(DINV));
+        CHECK(cudaFree(alphaopt));
+        CHECK(cudaFree(alphalimaps));
+        CHECK(cudaFreeHost(h_alphaopt));
+        CHECK(cudaFreeHost(h_alphalimaps));
+
+    }
 
     //FREE
-    CHECK(cudaFree(D));
-    CHECK(cudaFree(DINV));
-    CHECK(cudaFree(alphaopt));
     CHECK(cudaFree(s));
-    CHECK(cudaFree(alphalimaps));
 
     return 0;
 
